00b30ee840fc2f132e1e2f995d91f016c5058edd
alvaro-domatech <alvaro.braz@domatech.com.br>
2023-05-05 14:43:28 -0300

    fix: axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://dt-money-git-main-alvarobraz.vercel.app/',
+  baseURL: 'https://dt-money-alvarobraz.vercel.app/',
 })
