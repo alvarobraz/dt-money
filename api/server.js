15b19cc696ccbc06b235db7713c35173730d3555
alvaro-domatech <alvaro.braz@domatech.com.br>
2023-05-05 15:15:35 -0300

    feat:server

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 const { createServer } = require('http')
 const { exec } = require('child_process')
+const path = require('path')
+
+const serverJsonPath = path.resolve(__dirname, '..', 'server.json')
 
 createServer((req, res) => {
-  exec('json-server server.json -p 3333 -w -d 500', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-    console.error(`stderr: ${stderr}`)
-  })
+  exec(
+    `json-server ${serverJsonPath} -p 3333 -w -d 500`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      console.error(`stderr: ${stderr}`)
+    },
+  )
   res.end('Server started')
 }).listen()
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://dt-money-seven-orpin.vercel.app/api/server',
+  baseURL: 'https://dt-money-seven-orpin.vercel.app/api/server/',
 })
